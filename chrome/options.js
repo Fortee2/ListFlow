8d7637f8dcdf130c7adb0cdb1c20c4e652b49ad6
Randy Costner <costner.randy@gmail.com>
2024-02-18 11:42:46 -0500

    Update Prices Toggle

--- chrome/options.js
+++ chrome/options.js
@@ -3,12 +3,17 @@ document.getElementById('options-form').addEventListener('submit', function(even
 
   const preference1 = document.getElementById('serverURI').value;
   const preference2 = document.getElementById('createListingExports').checked;
+  const preference3 = document.getElementById('updatePrice').checked;
 
   chrome.storage.sync.set({ serverURI: preference1 }, function() {
     console.log('Preferences saved.');
   });
 
   chrome.storage.sync.set({ createExport: preference2 }, function() {
+    console.log('Preferences saved.');
+  });
+
+  chrome.storage.sync.set({ updatePrice: preference3  }, function() {
     console.log('Preferences saved.');
   });
 });
@@ -24,5 +29,9 @@ window.onload = function() {
 
     chrome.storage.sync.get('createExport', function(data) {
       document.getElementById('createListingExports').checked = data.createExport;
+    });
+
+    chrome.storage.sync.get('updatePrice', function(data) {
+      document.getElementById('updatePrice').checked = data.updatePrice;
     });
 }
