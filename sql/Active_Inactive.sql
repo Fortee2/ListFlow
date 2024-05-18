aa0d6a46b6b5c6ad7821d346354dbb575a6c16aa
Randy Costner <costner.randy@gmail.com>
2024-05-18 13:08:04 -0400

    Query Update

--- sql/Active_Inactive.sql
+++ sql/Active_Inactive.sql
@@ -1,32 +1,6 @@
-	Select l.ItemNumber, l.ItemTitle, l.Active,l.dateended, l.datesold,  ll.ItemNumber,  ll.ItemTitle, ll.lastupdated, ll.Active
-	from listflow.Listing l 
-		right join listflow.Listing ll on l.CrossPostId = ll.CrossPostId 
-	where (l.SalesChannelId = '28e91dfe-9a9d-482d-4aed-08db50d0bd42' or l.SalesChannelId is null) 
-    and (
-			ll.SalesChannelId = '1411f7bd-3872-4543-812c-f1b81569be89'  
-			or 
-			ll.SalesChannelId = 'd59f7ad3-1b2e-4347-bb4c-807d673d3536'
-			or 
-			ll.SalesChannelId is null 
-        )
-	and ll.Active  = 1
-    and (l.Active = 0)
-    union
-	Select l.ItemNumber, l.ItemTitle, l.Active,l.dateended, l.datesold, ll.ItemNumber, ll.ItemTitle, ll.lastupdated, ll.Active
-	from listflow.Listing l 
-		right join listflow.Listing ll on l.CrossPostId = ll.CrossPostId 
-	where 
-    (
-		l.SalesChannelId ='1411f7bd-3872-4543-812c-f1b81569be89'  
-        or
-        l.SalesChannelId = 'd59f7ad3-1b2e-4347-bb4c-807d673d3536'
-        or 
-        l.SalesChannelId is null
-	) 
-    and 
-    (ll.SalesChannelId = '28e91dfe-9a9d-482d-4aed-08db50d0bd42'  or ll.SalesChannelId is null )
-	and ll.Active  = 1
-    and (l.Active = 0);
+SELECT * FROM listflow.active_inactive
+order by datesold desc;    
+    
     
 select  *
 from listflow.Listing l
