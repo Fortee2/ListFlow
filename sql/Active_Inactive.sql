58baa0b9ca8e8cf1358a8e32196a6efe1ac10d2f
Randy Costner <costner.randy@gmail.com>
2024-11-22 08:09:21 -0500

    Added Poshmark Parsing

--- sql/Active_Inactive.sql
+++ sql/Active_Inactive.sql
@@ -1,23 +1,6 @@
 SELECT * FROM listflow.active_inactive
 order by datesold desc;    
 
-update listflow.Listing set active = 0 where itemnumber = '1363483067';
-
-UPDATE listflow.Listing set active = 0 where active = 1 and dateSold is not null;
-
 select * from listflow.Listing 
-where itemnumber = 'm22921426077'
+where itemnumber = 'm20598766426'
 order by dateSold desc;
-
-select * from listflow.SalesChannel;
-    
-
-update listflow.Listing set active =1, datesold = null where itemnumber ='m42834175894';
-
-update listflow.Listing set crosspostid = uuid() where itemnumber in ('m38029026701' , '186618464257' );
-
-select  *
-from listflow.Listing l
-where active = true
-and crosspostid is null
-order by itemtitle;
