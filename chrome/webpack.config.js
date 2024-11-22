357482ff5c193b247db8d435d87906e468e8a4ea
Randy Costner <costner.randy@gmail.com>
2024-11-22 08:21:05 -0500

    Conversion to Typescript - WIP

---
+++ chrome/webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+module.exports = {
+  entry: {
+    background: './src/background.ts',
+    // Add other entry points as needed
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].js'
+  },
+  resolve: {
+    extensions: ['.ts', '.js']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      }
+    ]
+  }
+};
