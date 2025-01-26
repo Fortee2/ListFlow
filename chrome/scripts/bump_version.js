1573c01db43f9ca4d7fe09d203ad6cc3a3ed3bfd
Randy Costner <costner.randy@gmail.com>
2025-01-25 20:24:31 -0500

    Update bump_version.js

--- chrome/scripts/bump_version.js
+++ chrome/scripts/bump_version.js
@@ -3,6 +3,14 @@ const manifest = require('../manifest.json');
 
 // Increment build in the version field (e.g., 1.0.0.1 → 1.0.0.2)
 const parts = manifest.version.split('.');
+let currentDate = new Date();
+parts[0] = currentDate.getFullYear().toString();
+parts[1] = (currentDate.getMonth() + 1).toString();
+if (parts[2] !== currentDate.getDate().toString()) {
+    parts[2] = currentDate.getDate().toString();
+    parts[parts.length - 1] = '0';
+}
+
 parts[parts.length - 1] = (parseInt(parts[parts.length - 1]) + 1).toString();
 manifest.version = parts.join('.');
 
